e72e7e44c16ab0e792fa2c2d6c13bc77ea8e49a0
Sergey Kholin <xainse@users.noreply.github.com>
2016-08-17 17:54:14 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 var config = {
-    //json_url: 'http://d3js-charts.appspot.com/test.json'
-    json_url: 'http://storage.googleapis.com/d3js-charts1.appspot.com/d3js-source-data.json'
+    json_url: 'http://d3js-charts.appspot.com/test.json'
+    //json_url: 'http://storage.googleapis.com/d3js-charts1.appspot.com/d3js-source-data.json'
 };
