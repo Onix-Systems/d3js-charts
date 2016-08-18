f4abf2295d2c18ee3e5e75a4af4f8788cf706db7
Eugen Dzubenko <1senjen1@gmail.com>
2016-08-18 15:28:18 +0300

    added error check for json loading

--- getJson.php
+++ getJson.php
@@ -1,4 +1,19 @@
 <?php
-$json = file_get_contents($_GET['url']);
-print_r($json);
+if($_GET['url']){
+    $ch = curl_init($_GET['url']);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $json = '';
+    if( ($json = curl_exec($ch) ) === false)
+    {
+        print_r('Curl error: ' . curl_error($ch));
+    }
+    else
+    {
+        print_r($json);
+    }
+    curl_close($ch);
+}else{
+    print_r('Url is required');
+}
+
 ?>
