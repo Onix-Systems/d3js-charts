0bbe9e44947134400e6b88f58e7523efda9faaf9
Eugen Dzubenko <1senjen1@gmail.com>
2016-08-19 16:27:32 +0300

    updated json's routs

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var config = {
-    json_url: 'http://d3js-test.net/test.json',
-    config_json: 'http://d3js-test.net/config.json'
+    json_url: 'https://storage.googleapis.com/d3js-charts1.appspot.com/test.json',
+    config_json: 'https://storage.googleapis.com/d3js-charts1.appspot.com/config.json'
     //json_url: 'http://d3js-charts.appspot.com/test.json'
     // json_url: 'http://storage.googleapis.com/d3js-charts1.appspot.com/d3js-source-data.json'
 };
