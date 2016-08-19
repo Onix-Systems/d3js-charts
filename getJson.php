46a3ec335b05e68976b9acc05ec64fecb102936b
Eugen Dzubenko <1senjen1@gmail.com>
2016-08-19 16:29:50 +0300

    revert getJson

--- getJson.php
+++ getJson.php
@@ -1,19 +1,4 @@
 <?php
-if($_GET['url']){
-    $ch = curl_init($_GET['url']);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $json = '';
-    if( ($json = curl_exec($ch) ) === false)
-    {
-        print_r('Curl error: ' . curl_error($ch));
-    }
-    else
-    {
-        print_r($json);
-    }
-    curl_close($ch);
-}else{
-    print_r('Url is required');
-}
-
+$json = file_get_contents($_GET['url']);
+print_r($json);
 ?>
