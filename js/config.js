6bdc3dc6242def8536643de3c54fb5e43c93abb9
Eugen Dzubenko <1senjen1@gmail.com>
2016-08-19 16:01:22 +0300

    updated design and charts logic

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 var config = {
-    // json_url: 'test.json'
+    json_url: 'http://d3js-test.net/test.json',
+    config_json: 'http://d3js-test.net/config.json'
     //json_url: 'http://d3js-charts.appspot.com/test.json'
-    json_url: 'http://storage.googleapis.com/d3js-charts1.appspot.com/d3js-source-data.json'
+    // json_url: 'http://storage.googleapis.com/d3js-charts1.appspot.com/d3js-source-data.json'
 };
